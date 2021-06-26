f69804c17072820e1190086db35c75b4171bcc27
OlLiashko <oliashko@danavero.com>
2021-06-26 17:57:59 +0300

    prepaire main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { FailRouteComponent } from './components/fail-route/fail-route.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    FailRouteComponent
+    FailRouteComponent,
+    GithubReposComponent
   ],
   imports: [
     BrowserModule,
